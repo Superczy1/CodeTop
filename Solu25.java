41755bec04778094ea8aa1f4291eef524a5ccdf6
AlexzyChu <1181835563@qq.com>
2021-10-02 22:08:11 +0800

    k 个一组反转链表

--- Solu25.java
+++ Solu25.java
@@ -1,44 +1,40 @@
-package CodeHot;
+package CodeTop;
 
 public class Solu25 {
     //反转k链表
     public static ListNode reverseKGroup(ListNode head, int k) {
         if (k == 1)
             return head;
-        ListNode H = new ListNode(0);
-        H.next = head;
+        ListNode hair = new ListNode(0);
+        hair.next = head;
+
+        int step = 0;
         ListNode p = head;
-        ListNode h = H;
-        ListNode start = H;
-        int count = 0;
+        ListNode pre = hair;
         while (p.next != null) {
             p = p.next;
-            count++;
-            if ((count + 1) % k == 0){
-                ListNode[] nodes = reverse(start.next, p);
-                h.next = nodes[0];
-                start = nodes[1];
+            step++;
+            if ((step + 1) % k == 0) {
+                ListNode[] nodes = reverse(pre.next, p);
+                pre.next = nodes[0];
+                pre = nodes[1];
                 p = nodes[1];
-                h = nodes[1];
             }
         }
-        ListNode[] nodes = reverse(start.next, p);
-        h.next = nodes[0];
-        return H.next;
+        return hair.next;
     }
 
     public static ListNode[] reverse(ListNode head, ListNode tail) {
         ListNode pre = tail.next;
         ListNode p = head;
-        ListNode Next = head;
-        ListNode end = tail;
-        while (pre != end) {
+        ListNode Next;
+        while (pre != tail) {
             Next = p.next;
             p.next = pre;
             pre = p;
             p = Next;
         }
-        return new ListNode[]{pre,head};
+        return new ListNode[]{pre, head};
     }
 
     public static void main(String[] args) {
