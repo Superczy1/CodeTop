761726f813f22f1666c39e024af64c8bdac52fc4
AlexzyChu <1181835563@qq.com>
2021-10-02 22:33:28 +0800

    test 测试用例

---
+++ test.java
@@ -0,0 +1,34 @@
+package CodeTop;
+
+import static CodeTop.Solu206.reverseList;
+import static CodeTop.Solu25.reverseKGroup;
+
+public class test {
+    public static void main(String[] args) {
+        test206();
+    }
+    //k 个一组反转链表
+    public static void test25(){
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+        for (int i = 0; i < 8; i++) {
+            ListNode n = new ListNode(i + 1);
+            p.next = n;
+            p = p.next;
+        }
+        head = reverseKGroup(head.next, 3);
+        System.out.println();
+    }
+    //反转链表
+    public static void test206(){
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+        for (int i = 0; i < 8; i++) {
+            ListNode n = new ListNode(i + 1);
+            p.next = n;
+            p = p.next;
+        }
+        head = reverseList(head.next);
+        System.out.println();
+    }
+}
