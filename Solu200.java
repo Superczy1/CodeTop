b3c0d58c1962551b7f66ef9e5c9cff76af3f239d
AlexzyChu <1181835563@qq.com>
2021-10-09 23:57:31 +0800

    mid 岛屿数量

---
+++ Solu200.java
@@ -0,0 +1,33 @@
+package CodeTop;
+
+/**
+ * AlexzyChu
+ * 2021/10/8 19:19
+ * <p>
+ * Descripition 岛屿数量
+ */
+public class Solu200 {
+    public int numIslands(char[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1'){
+                    dfs(i, j, grid);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+    public void dfs(int row, int col, char[][] grid){
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length)
+            return;
+        if (grid[row][col] == '1') {
+            grid[row][col] = '0';
+            dfs(row + 1, col, grid);
+            dfs(row - 1, col, grid);
+            dfs(row, col - 1, grid);
+            dfs(row, col + 1, grid);
+        }
+    }
+}
